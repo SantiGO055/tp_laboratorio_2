b1bba00bc95cb26967941d821aa602901afb9d53
SantiGO055 <santigonzalez05@gmail.com>
2020-05-11 01:08:14 -0300

    Subo tp terminado

---
+++ TP-02/Entidades/Camioneta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Camioneta : Vehiculo
+    {
+        public Camioneta(EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+        }
+        /// <summary>
+        /// Las camionetas son grandes
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        /// <summary>
+        /// Sobrecarga de metodo Mostrar que hereda de Vehiculo
+        /// </summary>
+        /// <returns>Retorna los datos de la camioneta junto con los datos de la clase vehiculo</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMIONETA");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : "+ this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    
+    public enum ETamanio
+    {
+        Chico, Mediano, Grande
+    }
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda
+        }
+        private EMarca marca;
+        private string chasis;
+        private ConsoleColor color;
+
+        public Vehiculo(EMarca marca, string chasis , ConsoleColor color)
+        {
+            this.marca = marca;
+            this.chasis = chasis;
+            this.color = color;
+        }
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CHASIS: " + this.chasis);
+            sb.AppendLine("MARCA : " + this.marca);
+            sb.AppendLine("COLOR : " + this.color);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Sobrecarga el casteo explicito a string retornando los datos del vehiculo
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Vehiculo p)
+        {
+            return p.Mostrar();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo vehiculo1, Vehiculo vehiculo2)
+        {
+            return (vehiculo1.chasis == vehiculo2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo vehiculo1, Vehiculo vehiculo2)
+        {
+            return (!(vehiculo1.chasis == vehiculo2.chasis));
+        }
+    }
+}
