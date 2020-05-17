6d0d510bb9d9bbcbfa6275dd22ce5eb380db5232
SantiGO055 <santigonzalez05@gmail.com>
2020-05-17 15:06:50 -0300

    Recuperatorio de TP2

---
+++ RecuperatoriosTP/TP2/Entidades/Estacionamiento.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public class Estacionamiento
+    {
+        List<Vehiculo> vehiculos;
+        int espacioDisponible;
+        public enum ETipo
+        {
+            Moto, Automovil, Camioneta, Todos
+        }
+
+        #region "Constructores"
+        private Estacionamiento()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+        public Estacionamiento(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el estacionamiento y TODOS los vehículos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        /// 
+        public static string Mostrar(Estacionamiento estacionamiento, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", estacionamiento.vehiculos.Count, estacionamiento.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Vehiculo vehiculoEstacionado in estacionamiento.vehiculos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Camioneta:
+                        if (vehiculoEstacionado is Camioneta)
+                        {
+                            sb.AppendLine(vehiculoEstacionado.Mostrar());
+                        }
+                        break;
+                    case ETipo.Moto:
+                        if (vehiculoEstacionado is Moto)
+                        {
+                            sb.AppendLine(vehiculoEstacionado.Mostrar());
+                        }
+                        break;
+                    case ETipo.Automovil:
+                        if (vehiculoEstacionado is Automovil)
+                        {
+                            sb.AppendLine(vehiculoEstacionado.Mostrar());
+                        }
+                        break;
+                    default:
+                        sb.AppendLine(vehiculoEstacionado.Mostrar());
+                        break;
+                }
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator +(Estacionamiento estacionamiento, Vehiculo vehiculo)
+        {
+            if (!(estacionamiento is null))
+            {
+                foreach (var vehiculoEstacionado in estacionamiento.vehiculos)
+                {
+                    if (vehiculoEstacionado == vehiculo)
+                        return estacionamiento;
+                }
+                if (estacionamiento.vehiculos.Count < estacionamiento.espacioDisponible)
+                {
+                    estacionamiento.vehiculos.Add(vehiculo);
+                }
+            }
+            return estacionamiento;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator -(Estacionamiento cEstacionamiento, Vehiculo pVehiculo)
+        {
+            foreach (Vehiculo v in cEstacionamiento.vehiculos)
+            {
+                if (v == pVehiculo)
+                {
+                    cEstacionamiento.vehiculos.Remove(v);
+                    break;
+                }
+            }
+
+            return cEstacionamiento;
+        }
+        #endregion
+    }
+}
